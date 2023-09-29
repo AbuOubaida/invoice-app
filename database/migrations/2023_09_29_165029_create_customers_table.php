ac9186f5fc7a87da7a75c38cafb5e70c7ec6395e
abuoubaida <abuoubaida36@gmail.com>
2023-09-30 00:15:23 +0600

    db and factory create done

--- database/migrations/2023_09_29_165029_create_customers_table.php
+++ database/migrations/2023_09_29_165029_create_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('firstname');
-            $table->string('lname');
+            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('address');
             $table->timestamps();
